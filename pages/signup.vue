83f32e14cff3e0beed76ef34553ca18c1486e05b
Phachara1001 <bossyoso789@gmail.com>
2023-03-09 12:31:47 +0700

    Third Commit

--- pages/signup.vue
+++ pages/signup.vue
@@ -30,7 +30,7 @@
                     <label for="newsletter" class="font-light text-gray-500 dark:text-gray-300">ฉันยอมรับ <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">นโยบาย และ ข้อกำหนดความเป็นส่วนตัว</a></label>
                   </div>
               </div>
-              <button  type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onclick="location.href='home'">signup</button>
+              <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" @click="$router.push('/home')">เข้าสู่ระบบ</button>
           </form>
       </div>
   </div>
