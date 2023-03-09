c6ab255c0460fbb57e38439a87aa0a8be5741247
Phachara1001 <bossyoso789@gmail.com>
2023-03-09 10:37:05 +0700

    first commit

---
+++ pages/home.vue
@@ -0,0 +1,67 @@
+<template>
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+      <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">เมนูสหกรณ์ by Baan Boss Minimart</h2>
+          <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">สามารถเลือกช็อปได้โดยการกดที่รูปภาพ แล้วเราจะนำพาท่านไปสู่หน้าสินค้า</p>
+      </div> 
+      <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+              <a href="shop">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.discordapp.net/attachments/1033412250230329347/1083063402395611148/Black_And_White_Modern_Vintage_Retro_Brand_Logo_5.png" alt="Bonnie Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Snack | ขนมขบเคี้ยว</a>
+                  </h3>
+                  <span class="text-gray-500 dark:text-gray-400">ขนมขบเคี้ยวยี่ห้อต่างๆ</span>
+              </div>
+          </div> 
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+              <a href="drink">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.discordapp.net/attachments/1033412250230329347/1083063402173304832/Black_And_White_Modern_Vintage_Retro_Brand_Logo_4.png" alt="Jese Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Drink | เครื่องดื่ม</a>
+                  </h3>
+                  <span class="text-gray-500 dark:text-gray-400">เครื่องดื่มต่างๆ ทัั้งน้ำอัดลม ชา นม</span>
+              </div>
+          </div> 
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+              <a href="candy">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.discordapp.net/attachments/1033412250230329347/1083063402647265310/Black_And_White_Modern_Vintage_Retro_Brand_Logo_6.png" alt="Michael Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Candy | ลูกอม</a>
+                  </h3>
+                  <span class="text-gray-500 dark:text-gray-400">ลูกอมต่างๆ กินหลังอาหารเพื่อล้างปาก</span>
+              </div>
+          </div> 
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+              <a href="sandwich">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.discordapp.net/attachments/1033412250230329347/1083064967399493713/Black_And_White_Modern_Vintage_Retro_Brand_Logo_8.png" alt="Sofia Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Sandwich | แซนวิซ</a>
+                  </h3>
+                  <span class="text-gray-500 dark:text-gray-400">แซนวิซหน้าต่างๆ เหมาะกับการกินเป็นอาหารเช้า</span>
+              </div>
+          </div>
+          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+              <a href="accessorie">
+                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://media.discordapp.net/attachments/1033412250230329347/1083063402953453608/Black_And_White_Modern_Vintage_Retro_Brand_Logo_7.png" alt="Michael Avatar">
+              </a>
+              <div class="p-5">
+                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      <a href="#">Accessories | อุปกรณ์การเรียน</a>
+                  </h3>
+                  <span class="text-gray-500 dark:text-gray-400">อุปกรณ์ต่างๆ ที่สำคัญสำหรับการเรียน</span>
+              </div>
+          </div>
+      </div>  
+  </div>
+</section>
+</template>
---
+++ pages/signup.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-8 h-8 mr-2" src="https://media.discordapp.net/attachments/1033412250230329347/1083033276094107708/Black_And_White_Modern_Vintage_Retro_Brand_Logo_2.png" alt="logo">
+          สหกรณ์
+      </a>
+      <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              ลงทะเบียน
+          </h2>
+          <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+              <div>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">กรอกอีเมล</label>
+                  <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
+              </div>
+              <div>
+                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">กรอกรหัสผ่าน</label>
+                  <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+              </div>
+              <div>
+                  <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ยืนยันรหัสผ่าน</label>
+                  <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required >
+              </div>
+              <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input id="newsletter" aria-describedby="newsletter" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required>
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label for="newsletter" class="font-light text-gray-500 dark:text-gray-300">ฉันยอมรับ <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">นโยบาย และ ข้อกำหนดความเป็นส่วนตัว</a></label>
+                  </div>
+              </div>
+              <button  type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onclick="location.href='home'">signup</button>
+          </form>
+      </div>
+  </div>
+</section>
+</template>
